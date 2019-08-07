5cfd0b72eb04594a344fb36f2128b1c5d6d86a45
etknbe@gmail.com <etknbe@gmail.com>
2019-08-07 16:40:07 +0900

    smile

--- smile.js
+++ smile.js
@@ -13,7 +13,7 @@ const circle = g.append('circle');
 //d3 method chaining
 circle
     .attr('r', size / 2)
-    .attr('fill', 'yellow')
+    .attr('fill', '#fdd0a2')
     .attr('stroke', 'black');
 
 const eyeSpacing = 40;
@@ -48,9 +48,9 @@ const browYOffset = browWidth * 2;
 
 const brows = g.append('g')
     .attr('transform', `translate(0, -${browYOffset})`);
-    
-    brows.transition().duration(2000).attr('transform', `translate(0, ${ -browYOffset - 20} )`)
-    .transition().duration(2000).attr('transform', `translate(0, ${ -browYOffset } )`);
+
+brows.transition().duration(2000).attr('transform', `translate(0, ${-browYOffset - 20} )`)
+    .transition().duration(2000).attr('transform', `translate(0, ${-browYOffset} )`);
 
 const leftEyeBrow = brows.append('rect')
     .attr('width', browWidth)
@@ -62,6 +62,31 @@ const rightEyeBrow = brows.append('rect')
     .attr('height', browHeight)
     .attr('x', browWidth * 2 - browWidth);
 
+// beard
 
+const unit = 12;
+lineData = [
+    [0, 0], [-unit * 2, 0], [-unit * 3, unit * 1], [-unit * 3, unit * 2],
+    [-unit * 2, unit * 1], [-unit * 2, unit * 3], [-unit * 1, unit * 2]
+    , [-unit * 1, unit * 3], [0, unit * 4]
+];
+p = d3.line()(lineData);
+const beard = g
+    .append("path")
+    .attr("d", p)
+    .attr("stroke", "black")
+    .attr("fill", "brown");
 
+beard.attr('transform', 'translate(0,30)');
+
+var points = d3.line()
+    .x(function(d) { return x(d[0]); })
+    .y(function(d) { return y(d[1]); });
+
+// points = d3.line()([[0, 0], [-unit * 2, 0], [-unit * 3, unit * 1], [0, unit]]);
+const upBeard = g
+    .append("path")
+    .attr("d", points([[0, 0], [-unit * 2, 0], [-unit * 3, unit * 1], [0, unit]]))
+    .attr("stroke", "black")
+    .attr("fill", "brown");
 
